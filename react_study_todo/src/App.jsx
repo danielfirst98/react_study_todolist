426aeac7cfeb75ca3875da5cc1b128fdeb9cbacb
danielfirst98 <danielfirst98@gmail.com>
2026-08-08 19:33:48 +0900

    Feat list toggle and list empty message

--- react_study_todo/src/App.jsx
+++ react_study_todo/src/App.jsx
@@ -18,15 +18,25 @@ function App() {
     };
     setTodoList([...todoList, newTodoList]);
   }
-  function DeletList (id) {
-    setTodoList(todoList.filter((list) => list.id !== id))
+  function DeletList(targetId) {
+    setTodoList(todoList.filter((list) => list.id !== targetId));
+  }
+  function ToggleList(targetId) {
+    const togglList = todoList.map((list) =>
+      list.id === targetId ? {...list, isDone : !list.isDone} : list
+    );
+    setTodoList(togglList);
   }
   console.log('출력할 리스트: ', todoList);
   return (
     <>
       <Nav name={name} />
       <InputList addList={AddList} />
-      <TodoList todoList={todoList} deleteList={DeletList}/>
+      <TodoList
+        todoList={todoList}
+        deleteList={DeletList}
+        toggleList={ToggleList}
+      />
     </>
   );
 }
--- react_study_todo/src/components/TodoList/TodoList.jsx
+++ react_study_todo/src/components/TodoList/TodoList.jsx
@@ -1,24 +1,35 @@
-function TodoList({ todoList, deleteList }) {
+import styles from './TodoList.module.css';
+
+function TodoList({ todoList, deleteList, toggleList }) {
   console.log('출력할 값 : ', todoList);
   return (
     <div>
       <h1>리스트 출력</h1>
-      <ul>
-        {todoList.map(
-          (list) => (
-            console.log(list.id, list.text),
-            console.log(list.isDone),
-            (
-              <li key={list.id}>
-                {list.text}
-                <button type="button" onClick={() => deleteList(list.id)}>
-                  삭제
-                </button>
-              </li>
-            )
-          ),
-        )}
-      </ul>
+      {todoList.length === 0 ? (
+        <p>할 일이 없네요...</p>
+      ) : (
+        <ul>
+          {todoList.map(
+            (list) => (
+              console.log(list.id, list.text),
+              console.log(list.isDone),
+              (
+                <li key={list.id}>
+                  <span
+                    className={`${styles.todoList} ${list.isDone ? styles.finish : ''}`}
+                    onClick={() => toggleList(list.id)}
+                  >
+                    {list.text}
+                  </span>
+                  <button type="button" onClick={() => deleteList(list.id)}>
+                    삭제
+                  </button>
+                </li>
+              )
+            ),
+          )}
+        </ul>
+      )}
     </div>
   );
 }
